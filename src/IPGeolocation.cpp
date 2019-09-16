3ba6e469dea4dd203e2259a0375cb26951b4249b
Dushyant Ahuja <dusht.ahuja@gmail.com>
2019-09-16 22:22:03 +0100

    Update IPGeolocation.cpp

--- src/IPGeolocation.cpp
+++ src/IPGeolocation.cpp
@@ -25,7 +25,7 @@ void IPGeolocation::updateStatus(){
   int r=0; //retry counter
   while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
       delay(100);
-      //Serial.print(".");
+      Serial.print(".");
       r++;
   }
 
@@ -34,39 +34,42 @@ void IPGeolocation::updateStatus(){
   //GET Data
   Link = "/timezone?apiKey="+_Key;
 
-  //Serial.print("requesting URL: ");
-  //Serial.println(host+Link);
+  Serial.print("requesting URL: ");
+  Serial.println(host+Link);
 
   httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
 
-  //Serial.println("request sent");
+  Serial.println("request sent");
 
   while (httpsClient.connected()) {
     String line = httpsClient.readStringUntil('\n');
     if (line == "\r") {
-      //Serial.println("headers received");
-      //Serial.println(line);
+      Serial.println("headers received");
+      Serial.println(line);
       break;
     }
   }
 
-  //Serial.println("reply was:");
-  //Serial.println("==========");
-  //httpsClient.readStringUntil('\n'); // The API sends an extra line with just a number. This breaks the JSON parsing, hence an extra read
+  Serial.println("reply was:");
+  Serial.println("==========");
+  httpsClient.readStringUntil('\n'); // The API sends an extra line with just a number. This breaks the JSON parsing, hence an extra read
   while(httpsClient.connected()){
     line = httpsClient.readString();
-    //Serial.println(line); //Print response
+    Serial.println(line); //Print response
   }
   DynamicJsonDocument doc(capacity);
   _Response = line;
   deserializeJson(doc, line);
-  const char* timezone = doc["timezone"];
-  _TimeZone = String(timezone);
-  int dst_savings = doc["dst_savings"];
-  int timezone_offset = doc["timezone_offset"]; 
+  _TimeZone = doc["timezone"].as<String>();
+  Serial.print("Time Zone: ");
+  Serial.println(_TimeZone);
+  int dst_savings = doc["dst_savings"].as<int>();
+  int timezone_offset = doc["timezone_offset"];
   _offset=dst_savings + timezone_offset;
+  Serial.print("DST Savings: ");
+  Serial.println(dst_savings);
 }
 
 String IPGeolocation::getTimezone(){
